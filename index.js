3a296052b37e8ee907ca45f8a824c56e081ddaeb
rafiul from office <rafiulofficial396@gmail.com>
2025-11-15 18:49:21 +0600

    feat(server): setup MongoDB client connection and added dotenv package to manage environment variables

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.abvmfph.mongodb.net/?appName=Cluster0`
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get("/", (req, res) => {
     res.send('Hello FinEase')
 })
+
+async function run() {
+    try {
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Running port is ${port}`);
